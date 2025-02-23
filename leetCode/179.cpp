36c4bad0ef0ff7ed50b9a833eb1f76d4389f188c
prachikkk101 <khandelwalprachi42@gmail.com>
2025-02-23 11:12:34 +0530

    Update 179.cpp

--- leetCode/179.cpp
+++ leetCode/179.cpp
@@ -1,5 +1,30 @@
-#include<iostream>
-using namespace std;
-int main(){
-    return 0;
-}
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        // Convert integers to strings
+        vector<string> strNums;
+        for (int num : nums) {
+            strNums.push_back(to_string(num));
+        }
+
+        // Sort the strings using a custom comparator
+        sort(strNums.begin(), strNums.end(), [](string& a, string& b) {
+            return a + b > b + a; // Compare concatenated strings to decide order
+        });
+
+        // If the largest number is "0", the result is just "0"
+        if (strNums[0] == "0") {
+            return "0";
+        }
+
+        // Join the strings to form the largest number
+        string result = "";
+        for (string& str : strNums) {
+            result += str;
+        }
+
+        return result;
+    }
+};
+
+
